1e72f202c14e2e6b1791fda85819adca3c43ccd7
pchlebda <piotrekchlebda@o2.pl>
2015-03-14 22:05:17 +0100

    First commit

--- SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
+++ SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "STRING");
+		model.addAttribute("message", "SUPER STRING");
 		return "hello";
 
 	}
