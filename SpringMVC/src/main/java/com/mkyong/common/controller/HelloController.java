410f86f6ea54887fe8ea51d202ba4b23d1ce0ef4
Matt <Matt@INCOGNITO03>
2015-04-13 13:39:34 +0200

    test

--- SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
+++ SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.mkyong.common.controller;
 
 
+import java.util.Random;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ public class HelloController {
 
     private static final String MY_LOGIN = "mateusz";
     private static final String MY_PASSWORD = "abc";
+    private int liczba_1;
 
     //Mapowanie dla strony domyslnej
     @RequestMapping(value = "/index")
@@ -21,28 +23,52 @@ public class HelloController {
     }
 
     //Mapowanie po wyslaniu formularza
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @RequestMapping(value = "/login", method = RequestMethod.POST) //
     public ModelAndView welcome(@RequestParam(value = "username") String login,
                                 @RequestParam(value = "password") String password) {
         ModelAndView model = new ModelAndView();
 
+        
         //Tutaj sprawdzamy poprawnosc loginu i hasla
         if (checkLoginAndPassword(login, password)) {
             //Jesli poprawne stosowna wiadomosc
             model.addObject("message", "Strona powitalna!");
-            model.setViewName("login");
+            model.setViewName("add"); //view set
         } else {
             //Jesli nie poprawne wiadomosc
             model.addObject("message", "Login albo haslo niepoprawne");
-            model.setViewName("index");
+            model.setViewName("login");
+            
         }
+        
+        
         return model;
+    }
+    
+    @RequestMapping(value = "/info")
+    public ModelAndView print()
+    {
+    ModelAndView model = new ModelAndView();
+    
+    model.addObject("number" , rand());
+    return model;
+    
     }
 
     //Funkcja sprawdza poprawnosc loginu i hasla
     public boolean checkLoginAndPassword(String login, String password) {
         return MY_LOGIN.equals(login) && MY_PASSWORD.equals(password);
     }
+    
+    public int rand (){
+        
+        
+        
+        Random r = new Random();
+        
+        liczba_1 = r.nextInt(20);
+        return liczba_1;
+    }
+    }
 
 
-}
