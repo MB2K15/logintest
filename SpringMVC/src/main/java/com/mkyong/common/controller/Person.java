6b9d6886b13a616e77317efb399ee6a0fe094fbf
Matt <Matt@INCOGNITO03>
2015-04-13 13:37:58 +0200

    test

---
+++ SpringMVC/src/main/java/com/mkyong/common/controller/Person.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mkyong.common.controller;
+
+
+
+/**
+ *
+ * @author Matt
+ */
+public class Person {
+    
+    
+    private String name;
+    private int age ;
+    private String Last_name;
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLast_name() {
+        return Last_name;
+    }
+
+    public void setLast_name(String Last_name) {
+        this.Last_name = Last_name;
+    }
+    
+    
+    
+}
