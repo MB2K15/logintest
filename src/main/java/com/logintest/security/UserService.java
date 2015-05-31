80e3b406c56944251de5662335b1d3bff8fe03e6
Matt <Matt@192.168.0.107>
2015-05-31 22:22:06 +0200

    user interface

---
+++ src/main/java/com/logintest/security/UserService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logintest.security;
+
+import java.util.List;
+
+/**
+ *
+ * @author Matt
+ */
+public interface UserService {
+	public void add(UserModel usr);
+	public void edit(UserModel usr);
+	public void delete(int usrId);
+	public UserModel getUser(int usrId);
+	public List getAllUsers();
+}
+
